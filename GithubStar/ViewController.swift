33c94ae5372985fe97110db0029c439c78a158d5
Yuankun Zhang <fmcocoa@gmail.com>
2016-08-31 22:19:15 +0800

    Initial Commit

---
+++ GithubStar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GithubStar
+//
+//  Created by Yuankun Zhang on 8/31/16.
+//  Copyright © 2016 Yuankun Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
